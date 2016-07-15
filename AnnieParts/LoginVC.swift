d2ef258e83e2f0da8e78285cbcee77cb8e385d8d
Jonathan Lam <lamj18@uci.edu>
2016-07-15 14:06:52 -0700

    initial commit

---
+++ AnnieParts/LoginVC.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  AnnieParts
+//
+//  Created by Jonathan Lam on 7/15/16.
+//  Copyright © 2016 Boyang. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var forgetPassButton: UIButton!
+    
+    // MARK: - View Loading Functions
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    // MARK: - IB Outlet Functions
+    
+    @IBAction func loginPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func forgetPasswordPressed(sender: UIButton) {
+        
+    }
+    
+    // MARK: - Main Functions
+    
+    func checkCredentials(){
+        if true { // Insert post request
+            self.performSegueWithIdentifier("pushToSearch", sender: self)
+        }else{
+            // Display error
+        }
+    }
+}
+
