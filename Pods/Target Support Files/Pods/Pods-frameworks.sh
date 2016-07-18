59f8321b6d7e7a9018d4f5833a66d81a81db5782
ryanyue123 <yue.ryan123@gmail.com>
2016-07-18 13:21:15 -0700

    Added popupcontroller pod

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -86,10 +86,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/DropDown.framework"
+  install_framework "Pods/PopupController.framework"
   install_framework "Pods/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/Alamofire.framework"
   install_framework "Pods/DropDown.framework"
+  install_framework "Pods/PopupController.framework"
   install_framework "Pods/SwiftyJSON.framework"
 fi
