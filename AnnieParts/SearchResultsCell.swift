67589726fa7800032bf068214b0ab4f1e5b1d2af
ryanyue123 <yue.ryan123@gmail.com>
2016-07-18 12:28:27 -0700

    Added button in search results table view
    removed delayContentTouches from table cells

---
+++ AnnieParts/SearchResultsCell.swift
@@ -0,0 +1,26 @@
+//
+//  SearchResultsCell.swift
+//  AnnieParts
+//
+//  Created by Ryan Yue on 7/18/16.
+//  Copyright © 2016 Boyang. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsCell: UITableViewCell {
+
+    @IBOutlet weak var addButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ AnnieParts/SearchResultsTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  SearchResultsTableViewController.swift
+//  AnnieParts
+//
+//  Created by Ryan Yue on 7/18/16.
+//  Copyright © 2016 Boyang. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delaysContentTouches = false
+        for view in self.tableView.subviews {
+            if view is UIScrollView {
+                (view as? UIScrollView)!.delaysContentTouches = false
+                break
+            }
+        }
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("searchResultsCell", forIndexPath: indexPath) as! SearchResultsCell
+        cell.addButton.tag = indexPath.row
+        cell.addButton.addTarget(self, action: #selector(SearchResultsTableViewController.addProductToCart(_:)), forControlEvents: .TouchUpInside)
+
+        return cell
+    }
+    
+    func addProductToCart(button: UIButton) {
+        print(button.tag)
+    }
+}
