c692bb542e1da0e85b87410661c84dc53fc042f1
Jonathan Lam <lamj18@uci.edu>
2016-07-28 12:27:31 -0700

    productDetail

--- AnnieParts/ProductDetailViewController.swift
+++ AnnieParts/ProductDetailViewController.swift
@@ -14,12 +14,27 @@ class ProductDetailViewController: UIViewController {
     @IBOutlet weak var mainScrollView: UIScrollView!
     @IBOutlet weak var imageCaroselScrollView: UIScrollView!
     
+    @IBOutlet weak var addToCartView: UIView!
+    @IBOutlet weak var changeQuantityView: UIView!
+    @IBOutlet weak var quantityLabel: UILabel!
+    
+    @IBOutlet weak var contentView: UIView!
+    
+    
+    @IBOutlet weak var aboutSelect: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addNib("AboutView", toView: self.contentView)
+        addTapGR(aboutSelect, action: Selector("switchToAbout:"))
         
         mainScrollView.contentSize = CGSizeMake(self.view.frame.width, 1000)
         mainScrollView.showsVerticalScrollIndicator = true
         mainScrollView.scrollEnabled = true
+        
+        self.addToCartView.layer.cornerRadius = 5.0
+        self.changeQuantityView.layer.cornerRadius = 5.0
         
         //imageCaroselScrollView.frame.size.width = self.view.frame.width * 0.8
         imageCaroselScrollView.auk.settings.placeholderImage = UIImage(named: "placeholder")
@@ -37,15 +52,22 @@ class ProductDetailViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func addNib(named: String, toView: UIView){
+        let nibView = NSBundle.mainBundle().loadNibNamed(named, owner: self, options: nil)[0] as! UIView
+        nibView.frame = toView.frame
+        toView.addSubview(nibView)
     }
-    */
+    
+    func switchToAbout(gr: UITapGestureRecognizer){
+        self.aboutSelect.subviews[0].removeFromSuperview()
+        self.addNib("VideoView", toView: self.contentView)
+    }
+    
+    private func addTapGR(view: UIView, action: Selector){
+        let gr = UITapGestureRecognizer(target: self, action: action)
+        view.addGestureRecognizer(gr)
+    }
+    
 
 }
