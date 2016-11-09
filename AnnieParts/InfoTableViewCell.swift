6146aca4a84d4e281820ccca0343918e7cf646fd
ryanyue123 <yue.ryan123@gmail.com>
2016-11-08 20:33:39 -0800

    syntax error fix

--- AnnieParts/InfoTableViewCell.swift
+++ AnnieParts/InfoTableViewCell.swift
@@ -18,10 +18,8 @@ class InfoTableViewCell: UITableViewCell {
         // Initialization code
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
+    override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
