8c7a815e1fdcff9976ecc40df3238bd2b9c647f2
ryanyue123 <yue.ryan123@gmail.com>
2017-08-03 11:31:30 +0800

    order cell sizing and selection

--- AnnieParts/AnnieParts/Controller/OrdersVC.swift
+++ AnnieParts/AnnieParts/Controller/OrdersVC.swift
@@ -19,15 +19,26 @@ class OrdersVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
-        
-//        order_list_request { (orders) in
-//            self.orders = orders
-//            self.tableView.reloadData()
+        let o1 = Order(order_id: 100, user_id: 100, serial_number: "slkfjdsdfj", total: 39, status: "shipping")
+        let o2 = Order(order_id: 100, user_id: 100, serial_number: "slkfjdsdfj", total: 39, status: "shipping")
+        let o3 = Order(order_id: 100, user_id: 100, serial_number: "slkfjdsdfj", total: 39, status: "shipping")
+        self.orders = [[o1, o2], [o3]]
+        tableView.reloadData()
+//        order_list_request { (success, orders) in
+//            if (success) {
+//                self.orders = orders
+//                self.tableView.reloadData()
+//            }
+//            else {
+//                performLogin(vc: self)
+//            }
 //        }
     }
     func configureTableView() {
         tableView.delegate = self
-        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.dataSource = self
+
+        self.automaticallyAdjustsScrollViewInsets = false
         let nib = UINib(nibName: "OrderCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "OrderCell")
     }
@@ -41,6 +52,7 @@ class OrdersVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        print(orders[segmentedControl.selectedSegmentIndex].count)
         return orders[segmentedControl.selectedSegmentIndex].count
     }
 
@@ -51,14 +63,13 @@ class OrdersVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
         return UITableViewCell()
     }
-
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let presenter = Presentr(presentationType: .fullScreen)
         if let vc = self.storyboard?.instantiateViewController(withIdentifier: "OrderInfoVC") as? OrderInfoVC {
             customPresentViewController(presenter, viewController: vc, animated: true, completion: nil)
         }
+        self.tableView.deselectRow(at: indexPath, animated: true)
     }
-
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 1
     }
